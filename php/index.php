b17ccb5dc37e85d204c34c4d84caaa960895b2d9
Julia Cajeu <61912340+cajeu@users.noreply.github.com>
2023-05-29 21:31:08 -0300

    Update index.php - Deixando o site responsivo para mobile

--- php/index.php
+++ php/index.php
@@ -2,114 +2,128 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=chrome">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>FlexiHire</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Teste</title>
 </head>
 <body>
-    <div class="container-fluid Nav">
-        <div class="container-md">
+    <header>
+        <div class="container-fluid header">
             <div class="row">
-                <div class="col-xl-10 col-lg-12 col-md-12 my-xl-1 titulo">
+                <div class="col-md-1 col-sm-12 my-3">
+                </div>
+                <div class="col-md-7 col-sm-12 my-sm-3 text-sm-start text-center ps-sm-5 ps-0 titulo">
                     <h3>FlexiHire</h3>
                 </div>
-                <div class="col-xl-1 col-lg-12 col-md-12 my-xl-1 link">
+                <div class="col-md-2 col-sm-12 my-sm-4 ps-0 text-sm-start text-center link">
                     <a class="nav-link" href="#">Cadastro</a>
                 </div>
-                <div class="col-xl-1 col-lg-12 col-md-12 my-xl-1 link">
+                <div class="col-md-2 col-sm-12 my-sm-4 mb-4 ps-0 text-sm-start text-center link">
                     <a class="nav-link" href="#">Login</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container-fluid degrade">
+        <div class="row conteudo">
+            <div class="col-md-1 col-sm-12">
+            </div>
+            <div class="col-md-5 col-sm-12 text-sm-start px-sm-5 px-0 text-center textoCont">
+                <h2 class="">Encontre sua taxa!</h2>
+                <p >Ajudando estabelecimentos e funcionários a encontrarem o melhor match para trabalhos temporários em bares e restaurantes.</p>
+            </div>
+            <div class="col-md-5 col-sm-12 imgCont">
+                <img src="./imgs/Coffee shop-bro (4).png" class="img-fluid float-end imagemHeader ">
+            </div>
+            <div class="col-md-1 col-sm-12">
             </div>
         </div>
     </div>
-    </div>
-    <div class="container-fluid Header">
-        <div class="container-md">
-            <div class="row">
-                <div class="col-md-4 textoHeader">
-                    <h2>Encontre sua taxa!</h2>
-                    <p>Ajudando estabelecimentos e funcionários a encontrarem o melhor match para trabalhos temporários em bares e restaurantes</p>
-                </div> 
-                <div class="col-md-8 ">
-                    <img src="./imagens/Coffee shop-bro (4).png" class="img-fluid float-end imagemHeader ">
-                </div> 
-            </div>
-        </div>
-    </div>
-        <div class="container-fluid Body">
-            <div class="container-md textoBody text-center">
-                <h2>Prático e Seguro para todas as partes!</h2>
-                <h5>Acompanhamos você do cadastro da vaga até o pagamento</h5>
-                <div class="row Cards">
-                    <div class="col-sm-3 mb-3 mb-sm-0">
-                      <div class="card ">
-                        <div class="card-body card1">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-search-heart" viewBox="0 0 16 16">
-                                <path d="M6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018Z"/>
-                                <path d="M13 6.5a6.471 6.471 0 0 1-1.258 3.844c.04.03.078.062.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1.007 1.007 0 0 1-.1-.115h.002A6.5 6.5 0 1 1 13 6.5ZM6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11Z"/>
-                            </svg>                                
-                          <p class="card-text">Encontre o que precisa.</p>
-                        </div>
-                      </div>
+    <div class="container-fluid fundoVerde">
+        <div class="container-md textoBody text-center ">
+            <h2>Prático e Seguro para todas as partes!</h2>
+            <h5>Acompanhamos você do cadastro da vaga até o pagamento</h5>
+            <div class="row my-4 mx-4">
+                <div class="col-sm-3 mb-3 mb-sm-0">
+                  <div class="card border-0 ">
+                    <div class="card-body rounded-3 card1">
+                      <h5 class="card-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-search-heart" viewBox="0 0 16 16">
+                            <path d="M6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018Z"/>
+                            <path d="M13 6.5a6.471 6.471 0 0 1-1.258 3.844c.04.03.078.062.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1.007 1.007 0 0 1-.1-.115h.002A6.5 6.5 0 1 1 13 6.5ZM6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11Z"/>
+                        </svg>
+                      </h5>
+                      <p class="card-text">Encontre o que precisa.</p>
                     </div>
-                    <div class="col-sm-3">
-                      <div class="card">
-                        <div class="card-body card2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-chat-square-text" viewBox="0 0 16 16">
-                                <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                                <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-                            </svg>
-                          <p class="card-text"> Veja as reviews, deixe a sua!</p>
-                        </div>
-                      </div>
+                  </div>
+                </div>
+                <div class="col-sm-3 mb-3  mb-sm-0">
+                  <div class="card border-0">
+                    <div class="card-body rounded-3 card2 ">
+                      <h5 class="card-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-chat-square-text" viewBox="0 0 16 16">
+                            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                            <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+                        </svg>
+                      </h5>
+                      <p class="card-text">Veja as reviews, deixe a sua!</p>
                     </div>
-                    <div class="col-sm-3">
-                        <div class="card">
-                          <div class="card-body card3">
+                  </div>
+                </div>
+                <div class="col-sm-3 mb-3 mb-sm-0">
+                    <div class="card border-0 ">
+                      <div class="card-body rounded-3 card3">
+                        <h5 class="card-icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-briefcase" viewBox="0 0 16 16">
                                 <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5zm1.886 6.914L15 7.151V12.5a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5V7.15l6.614 1.764a1.5 1.5 0 0 0 .772 0zM1.5 4h13a.5.5 0 0 1 .5.5v1.616L8.129 7.948a.5.5 0 0 1-.258 0L1 6.116V4.5a.5.5 0 0 1 .5-.5z"/>
                             </svg>  
-                            <p class="card-text">Feche parcerias de trabalho.</p>
-                          </div>
-                        </div>
+                        </h5>
+                        <p class="card-text">Feche parcerias de trabalho</p>
                       </div>
-                      <div class="col-sm-3">
-                        <div class="card">
-                          <div class="card-body card4">
+                    </div>
+                  </div>
+                  <div class="col-sm-3 mb-3 mb-sm-0">
+                    <div class="card border-0">
+                      <div class="card-body rounded-3 card4">
+                        <h5 class="card-icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-ui-checks" viewBox="0 0 16 16">
                                 <path d="M7 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-1zM2 1a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm0 8a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-2a2 2 0 0 0-2-2H2zm.854-3.646a.5.5 0 0 1-.708 0l-1-1a.5.5 0 1 1 .708-.708l.646.647 1.646-1.647a.5.5 0 1 1 .708.708l-2 2zm0 8a.5.5 0 0 1-.708 0l-1-1a.5.5 0 0 1 .708-.708l.646.647 1.646-1.647a.5.5 0 0 1 .708.708l-2 2zM7 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-1zm0-5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 8a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
                             </svg>
-                            <p class="card-text">Cadastro rápido e intuitivo.</p>
-                          </div>
-                        </div>
+                        </h5>
+                        <p class="card-text">Cadastro rápido e intuitivo.</p>
                       </div>
+                    </div>
                   </div>
-            </div>
+              </div>
         </div>
-    <div class="container-fluid Cadastro">
-        <div class="container-md">
-            <div class="row">
-                <div class="col-md-6 textoCadastro">
-                    <h5>Faça parte da comunidade:</h5>
-                </div>
-                <div class="col-md-3 Botao">
-                    <a href="#" class="btn btn-primary">Cadastro de <br/>Funcionário</a>
-                </div>  
-                <div class="col-md-3 Botao">
-                    <a href="#" class="btn btn-primary">Cadastro de Estabelecimento</a>
-                </div>         
+    </div>
+    <div class="container-fluid degrade">
+        <div class="row">
+            <div class="col-md-1 col-sm-12 my-3">
+            </div>
+            <div class="col-md-4 col-sm-12 my-sm-3 py-sm-3 text-sm-start text-center ps-sm-5 textoCadastro">
+                <h4>Faça parte da comunidade:</h4>
+            </div>
+            <div class="col-md-3 col-sm-12 my-sm-4 text-sm-start text-center py-sm-0 py-3">
+                <a href="#" class="btn btn-primary border-0">Cadastro de <br/>Funcionário</a>
+            </div>
+            <div class="col-md-3 col-sm-12 my-sm-4 mb-4 text-sm-start text-center py-sm-0 py-3">
+                <a href="#" class="btn btn-primary border-0">Cadastro de <br/>Estabelecimento</a>
             </div>
         </div>
     </div>
-    <div class="container-fluid footer">
-        <div class="container-md">
-            <div class="row">
-                <div class="col-md-8"></div>
+    <footer class="container-fluid footer">
+        <div class="row">
+            <div class="col-sm-11 text-sm-end text-center">
                 <p>Fale conosco: contato@flexihire.com</p>
             </div>
+            <div class="col-sm-1">
+            </div>
         </div>
-    </div>
+        </div>  
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
+</html>
